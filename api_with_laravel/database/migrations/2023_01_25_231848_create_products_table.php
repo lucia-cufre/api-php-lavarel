ab5253c1ced6a310cfaef51cbbc20e2629962bf6
Lucia Cufre Aman <luchicufre@hotmail.com>
2023-01-25 21:59:36 -0300

    Database connected and migrations created

---
+++ api_with_laravel/database/migrations/2023_01_25_231848_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('category');
+            $table->enum('status', ['ACTIVE', 'INACTIVE']);
+            $table->integer('quantity');
+            $table->date('created_at');
+            $table->date('update_at')->nullable();
+            $table->date('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
